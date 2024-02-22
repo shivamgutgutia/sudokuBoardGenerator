e733b9dab3a2ecf9db26534e373dbc894d1e30d6
Shivam Gutgutia <gutgutia159@gmail.com>
2024-02-22 22:57:01 +0530

    Added output format option

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from boardGenerator import *
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
 
 app=Flask(__name__)
 
@@ -11,9 +11,13 @@ def home():
 @app.route("/generate",methods=["GET"])
 def generateRoute():
     difficulty = request.args.get('difficulty', 'easy')
+    outputFormat = request.args.get("output_format","UI")
     board = np.zeros((9,9),dtype=int)
     question,solution=generate(board.copy(),difficulty=difficulty)
-    return render_template('output.html', question=question.tolist(), solution=solution.tolist())
+    if(outputFormat=="UI"):
+        return render_template('output.html', question=question.tolist(), solution=solution.tolist())
+    else:
+        return jsonify({"question":question.tolist(),"solution":solution.tolist()})
 
 if __name__ =="__main__":
     app.run()
